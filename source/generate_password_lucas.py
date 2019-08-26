b44066c5261c7f7df731f270e29fc04ef4a2b859
Lucas <lucas.msampaio@outlook.com>
2019-08-26 11:56:40 -0300

    Segundo commit, com correções.

--- source/generate_password_lucas.py
+++ source/generate_password_lucas.py
@@ -1,3 +1,10 @@
+# coding: utf-8
+
+###################### 26-08-2019
+########### Lucas Sampaio de Melo
+####### generate_password_lucas.py
+#### v1.0 
+
 import random
 from random import randint
 
@@ -8,7 +15,7 @@ def random_pass():
     password = ''
     for i in range(0,length,2):
         password += random.choice(alpha)
-    print(password)
+    return password
 
 if __name__ == "__main__":
     random_pass()
